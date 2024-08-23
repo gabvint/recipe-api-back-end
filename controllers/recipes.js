89afc90d175ab2ab1d4cb7914ed93d1ef1710836
gabtongol <geybtongol@gmail.com>
2024-08-23 19:02:45 -0400

    update and delete working

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -49,4 +49,46 @@ router.get('/:recipeId', async (req, res) => {
     }
 })
 
+// edit a specific recipe
+router.put('/:recipeId', async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.recipeId);
+
+        // checks if the recipe author is authorized to update
+        if (!recipe.author.equals(req.user._id)) {
+            return res.status(403).send("You are not authorize to do that!");
+        }
+
+        const updatedRecipe = await Recipe.findByIdAndUpdate(
+            req.params.recipeId,
+            req.body,
+            { new: true }
+          );
+      
+        updatedRecipe._doc.author = req.user;
+
+        res.status(200).json(updatedRecipe);
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// delete a specific recipe 
+router.delete('/:recipeId', async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.recipeId)
+
+        if (!recipe.author.equals(req.user._id)) {
+            return res.status(403).send ("You are not authorize to do that!");
+        }
+
+        const deletedRecipe = await Recipe.findByIdAndDelete(req.params.recipeId)
+
+        res.status(200).json(deletedRecipe)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router;
