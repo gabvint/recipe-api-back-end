a0b50aab999629c8925333c6187321f478667df7
gabtongol <geybtongol@gmail.com>
2024-08-23 18:48:18 -0400

    get recipes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -6,26 +6,11 @@ const router = express.Router();
 
 router.use(verifyToken);
 
-// // create hoots
-// router.post('/', async (req, res) => {
-//     try {
-//       req.body.author = req.user._id;
-//       const hoot = await Hoot.create(req.body);
-//       hoot._doc.author = req.user;
-//       res.status(201).json(hoot);
-
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).json(error);
-
-//     }
-// });
-
 
 // create recipes 
 router.post('/', async (req, res) => {
     try {
-        
+
         req.body.author = req.user._id
         const recipe = await Recipe.create(req.body)
         recipe._doc.author = req.user 
@@ -33,6 +18,33 @@ router.post('/', async (req, res) => {
 
     } catch (error) {
         console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+// get all recipes
+router.get('/', async (req, res) => {
+    try {
+        const recipe = await Recipe.find({})
+         .populate('author')
+         res.status(200).json(recipe)
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+});
+
+// get a specific recipe
+router.get('/:recipeId', async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.recipeId).populate([
+            'author', 
+            'ingredients',
+        ])
+
+        res.status(200).json(recipe)
+    } catch (error) {
         res.status(500).json(error)
     }
 })
