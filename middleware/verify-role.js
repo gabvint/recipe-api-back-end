e03a8e39a7c7a5cd59fb070c21a4b1e46d6d3a80
Gabrielle Tongol <geybtongol@gmail.com>
2025-07-30 20:29:26 +0800

    access control log

--- middleware/verify-role.js
+++ middleware/verify-role.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken');
+const logEvent = require('../utils/logEvents');
+const Log = require('../models/logs.js');
 
 function verifyRole (allowedRoles) {
-    return (req, res, next) => {
-        const userRole = req.user.role; 
-
-        if (!allowedRoles.includes(userRole)){
-            return res.status(403).json({ error: 'Forbidden: Insufficient role' });
-        }
-
+    return async (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        await logEvent({
+        user: req.user?._id || null,
+        action: 'unauthorized access',
+        status: 'failure',
+        details: `Insufficient role for ${req.originalUrl}`,
+        ip
+      });
+      return res.status(403).json({ error: 'Forbidden: Insufficient role' });
+    }
         next();
     };
 }
